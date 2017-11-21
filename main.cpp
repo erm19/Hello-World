4deb8994fa750e99a9d86d00a431238400f6fc65
eyal moskowitz <eyal.moskowitz@gmail.com>
2017-11-21 13:25:25 +0200

    don't work on the same file

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "yaaaaaaaaaaaaaaaaaaaaaaaaaay" << endl;
+    cout << "never work on the same class at the same time" << endl;
     return 0;
 }
 
